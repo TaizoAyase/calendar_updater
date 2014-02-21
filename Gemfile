8319c0dc825ec0d607d301b85779d1f1aed5b604
Taizo_Ayase <zxcvbnmkind@gmail.com>
2014-02-22 04:33:54 +0900

    Success to auth.
    Added setup: dumping configs to yaml file
    once the yaml file is generated by setup.rb,
    users do not have to touch the auth info

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@
 source "https://rubygems.org"
 
 gem "google-api-client",  "0.7.1"
+gem "google-api", "0.4.0"
 gem "nokogiri", "1.6.1"
 gem "launchy"
--- setup.rb
+++ setup.rb
@@ -1,7 +1,8 @@
 # coding: utf-8
 
-require 'bundler'
-Bundler.require
+require 'google/api_client'
+require 'Launchy'
+require 'yaml'
 
 key_file = Dir.glob("client_secret_*.apps.googleusercontent.com.json")
 raise unless key_file.length == 1
@@ -11,7 +12,7 @@ client_secrets = Google::APIClient::ClientSecrets.load
 
 client = Google::APIClient.new
 client.authorization.client_id = client_secrets.client_id
-client.authorization.client_id = client_secrets.client_secret
+client.authorization.client_secret = client_secrets.client_secret
 client.authorization.scope = "https://www.googleapis.com/auth/calendar"
 client.authorization.redirect_uri = client_secrets.redirect_uris.first
 
@@ -22,3 +23,17 @@ Launchy.open(uri)
 $stdout.write  "Enter authorization code: "
 client.authorization.code = gets.chomp
 client.authorization.fetch_access_token!
+
+# dump configs to yaml file
+hash = Hash.new
+hash[:client_id] = client.authorization.client_id
+hash[:client_secret] = client.authorization.client_secret
+hash[:scope] = client.authorization.scope
+hash[:redirect_uri] = client.authorization.redirect_uri
+hash[:refresh_token] = client.authorization.refresh_token
+hash[:access_token] = client.authorization.access_token
+
+str = YAML.dump(hash)
+File.write("config.yaml", str)
+
+
