cdd903f1035b48799d2eeaf9d0787bc38b568e70
Taizo_Ayase <zxcvbnmkind@gmail.com>
2014-03-01 17:24:05 +0900

    rename events to event
    because the evetns class has only one event information

--- event.rb
+++ event.rb
@@ -4,13 +4,24 @@ require 'bundler'
 Bundler.require
 require 'open-uri'
 
-class Events
+class Event
   CONFIG = YAML.load_file("./config.yaml")
-  
-  def self.get_events
 
+  # get evets list from web site
+  # return the ary of Event
+  def self.get_events
+    doc = Nokogiri::HTML(open(CONFIG[:target_url]))
+    doc.css("div > table > tbody > tr").each do |tr|
+      ary_tmp = []
+      tr.children.each do |td|
+        ary_tmp << td.content.encode('utf-8')
+      end
+
+    end
   end
 
+  # initialize with;
+  # hash[:date > :month/:day],[:time > :hour/:min],[:people],[:place]
   def initialize(hash)
     @date = hash[:date]
     @time = hash[:time]
@@ -25,7 +36,7 @@ class Events
         'dateTime' => Time.utc(0, @time[:min], @time[:hour] - 9 , @date[:day], @date[:month], CONFIG[:year], nil, nil, false, nil).iso8601
       },
       'end' => {
-        'dateTime' => Time.utc(0, @time[:min], @time[:hour] - 9 + 2, @date[:day], @date[:month], CONFIG[:year], nil, nil, false, nil).iso8601
+        'dateTime' => Time.utc(0, @time[:min], @time[:hour] - 9 + CONFIG[:duration], @date[:day], @date[:month], CONFIG[:year], nil, nil, false, nil).iso8601
       },
       'location' => @place
     }  	
